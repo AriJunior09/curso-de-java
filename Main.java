a6667db595cc386c6d0192395b36eab36fd53941
Ari Júnior <arioliveirajuniorjose@gmail.com>
2023-09-02 18:11:37 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int i;
+
+        for (i = 1; i <= n; i++){
+            int primeiro = i;
+            int segundo = i * i;
+            int terceiro = i * i * i;
+
+            System.out.printf("%d %d %d%n", primeiro, segundo, terceiro);
+
+        }
+
+        
+        sc.close();
+     
+    }
+	
+}
