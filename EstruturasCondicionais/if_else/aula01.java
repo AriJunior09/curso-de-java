e19c22157210f8fe3ddf5da7ea88a4c8867f4307
Ari Júnior <arioliveirajuniorjose@gmail.com>
2024-01-16 19:11:33 -0300

    atualizando aula01

--- EstruturasCondicionais/if_else/aula01.java
+++ EstruturasCondicionais/if_else/aula01.java
@@ -1,20 +1,28 @@
 package if_else;
 
+import java.util.Scanner;
+
 public class aula01 {
 
     public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um número de 1 à 9: ");
+        int n = sc.nextInt();
     	
-    	int x = 5;
-    	
-    	System.out.println("Bom dia");
-    	
-    	if (x > 10) {
-    		System.out.println("Boa tarde");
+    	if (n < 4) {
+    		System.out.println("Seu número está entre 1 e 3.");
     	}
-    	
-    	
-    	System.out.println("Bom noite");
-    	
+        else if( n < 7 ){
+            System.out.println("Seu número está entre 4 e 6.");
+        }
+        else if( n == 7 ){
+            System.out.println("Seu número é 7.");
+        }
+        else if( n > 7 ){
+        System.out.println("Seu número está entre 8 e 9.");
+        }
+
+        sc.close();
     	
     }
 }
