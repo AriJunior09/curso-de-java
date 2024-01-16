acc02fee3e5f0372816e422b5b08ce5d782640cf
Ari Júnior <arioliveirajuniorjose@gmail.com>
2024-01-16 19:39:36 -0300

    atualizando aula02

--- EstruturasCondicionais/if_else/aula02.java
+++ EstruturasCondicionais/if_else/aula02.java
@@ -7,13 +7,13 @@ public class aula02 {
 
         /*___________________    2 NOTAS DE ALUNO PARA ANALISAR    __________________________ */
 
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
 
         System.out.print("Entre com a primeira nota: ");
-        double nota1 = sc.nextDouble();
+        double nota1 = sc.nextDouble();   // Usuário coloca a primeira nota
         System.out.print("Entre com a segunda nota: ");
-        double nota2 = sc.nextDouble();
-        double m = media(nota1, nota2);
+        double nota2 = sc.nextDouble();   // Usuário coloca a segunda nota
+        double m = media(nota1, nota2);   // Criando uma variável "m" e chamanda o método "média"
 
         if (m < 5){
             System.out.print("Sua média foi de: " + m + " e você ficou Reprovado!");
@@ -29,8 +29,8 @@ public class aula02 {
     }
 
 
-    public static double media(double x, double z){
-        return (x + z ) / 2;
+    public static double media(double x, double z){ // Criando um metodo "media"
+        return (x + z ) / 2;  // Explicando para o programa o que eu quero que o método retorne
     }
     
 }
