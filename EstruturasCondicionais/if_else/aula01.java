7529a8a3a7ea0993fb4e1b022e35de20ff0c086c
Ari Júnior <arioliveirajuniorjose@gmail.com>
2024-01-16 19:01:16 -0300

    add aula01

---
+++ EstruturasCondicionais/if_else/aula01.java
@@ -0,0 +1,20 @@
+package if_else;
+
+public class aula01 {
+
+    public static void main(String[] args) {
+    	
+    	int x = 5;
+    	
+    	System.out.println("Bom dia");
+    	
+    	if (x > 10) {
+    		System.out.println("Boa tarde");
+    	}
+    	
+    	
+    	System.out.println("Bom noite");
+    	
+    	
+    }
+}
