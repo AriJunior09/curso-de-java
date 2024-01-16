088b1beb0071186017042f6797377fa162cac40d
Ari Júnior <arioliveirajuniorjose@gmail.com>
2024-01-16 20:20:24 -0300

    add aula03

---
+++ EstruturasCondicionais/if_else/aula03.java
@@ -0,0 +1,38 @@
+package if_else;
+
+import java.util.Scanner;
+
+public class aula03 {
+    public static void main(String[] args) {
+
+        //---------------   ANALISANDO O TIPO DE TRIÂNGULO --------------------//
+   
+        Scanner sc = new Scanner(System.in);
+        int r1, r2, r3;
+        System.out.print("Digite o valor da Reta A: ");
+        r1 = sc.nextInt();
+        System.out.print("Digite o valor da Reta B: ");
+        r2 = sc.nextInt();
+        System.out.print("Digite o valor da Reta C: ");
+        r3 = sc.nextInt();
+
+        if (((r1 + r2) > r3) && ((r2 + r3) > r1) && ((r3 + r1) > r2)){
+            System.out.println("Essas 3 retas podem formar um Triângulo!");
+        
+            if ((r1 == r2) && (r1 == r3) && (r2 == r3)){
+            System.out.println("O Triângulo é Equilátero!");
+            }
+            else if ((r1 == r2 && r2 != r3) || (r2 == r3 && r2 != r1)){
+            System.out.println("O Triângulo é Isósceles!");
+            }
+            else{
+            System.out.println("O Triângulo é Escaleno!");
+            }
+        }
+        else{
+        System.out.println("Não forma um triângulo!");
+        }
+
+        sc.close();
+    }
+}
