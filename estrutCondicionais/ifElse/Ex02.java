0ebe35a7061b269f3d3989551d09964832b0bdfa
Ari Júnior <arioliveirajuniorjose@gmail.com>
2024-01-28 18:30:57 -0300

    update pastas

--- estrutCondicionais/ifElse/Ex02.java
+++ estrutCondicionais/ifElse/Ex02.java
@@ -5,6 +5,10 @@ public class Ex02 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		/** O programa vai pedir ao usuário para digitar uma hora e a partir daí mostrará 3    possíveis respostas:
+        * BOM DIA, (se a hora for menor que 12);
+        * BOA TARDE, (se a hora estiver entre 12 e 18);
+        * BOA NOITE, (se a hora for acima de 18); */
 		
 		int hora;  /// Criando a variável hora para receber um valor.
 		
