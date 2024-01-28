406fd3381f025beb302f814b1fc45d3153f48ee5
Ari Júnior <arioliveirajuniorjose@gmail.com>
2024-01-28 18:19:52 -0300

    Update ex01

--- EstruturasCondicionais/ex01/Main.java
+++ EstruturasCondicionais/ex01/Main.java
@@ -5,19 +5,18 @@ public class Main {
    
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
+			System.out.print("Digite o número: ");
 			int n = sc.nextInt();
 			int i;
 
 			for (i = 1; i <= n; i++){
-			    int primeiro = i;         // Insira um valor //
-			    int segundo = i * i;      // Mostra o quadrado //
-			    int terceiro = i * i * i; // Mostra o cubo //
-				
+			    int primeiro = i;         // Mostra o valor 
+			    int segundo = i * i;      // Mostra o quadrado 
+			    int terceiro = i * i * i; // Mostra o cubo 
 				
 			    System.out.printf("%d %d %d%n", primeiro, segundo, terceiro);
 
 			}
-
 			
 			sc.close();
         }
