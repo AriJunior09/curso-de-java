3b97b213ec99d1b3a0b25e54633b8a85676e84ac
Ari Júnior <arioliveirajuniorjose@gmail.com>
2024-01-28 18:12:32 -0300

    Add ex04

---
+++ revisao/secao04/Ex04.java
@@ -0,0 +1,29 @@
+package revisao.secao04;
+
+import java.util.Scanner;
+
+public class Ex04 {
+    public static void main(String[] args) {
+		/*  Programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. */
+
+		Scanner sc = new Scanner(System.in);
+		double horas, valorHora, salario;
+		
+		System.out.print("Número do Fucionário: ");
+		int num = sc.nextInt();
+		System.out.print("Quantidade de horas trabalhadas: ");
+		horas = sc.nextDouble();
+		System.out.print("Qual o valor da hora? R$ ");
+		valorHora = sc.nextDouble();
+		
+		salario = horas * valorHora;
+		
+		System.out.println("O funcionário de inscrição: " + num + " vai ter um salário de: ");
+		System.out.printf("R$ %.2f%n", salario);
+		
+		
+		sc.close();
+
+	}
+
+}
