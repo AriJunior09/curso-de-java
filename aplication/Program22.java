b216c2d42f0900b1d062faa97ab4a6dbb54a053a
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-04-09 21:00:55 -0300

    "Update Program22"

--- aplication/Program22.java
+++ aplication/Program22.java
@@ -13,6 +13,15 @@ public class Program22 {
 
     System.out.println("O número digitado foi: " + numero);
 
+    int teste1 = numero;
+    System.out.println("Teste1 = " + teste1);
+    System.out.print("Digite o Teste2: ");
+    int teste2 = sc.nextInt();
+    System.out.println("Teste2 = " + teste2);
+
+    int teste3 = teste1 * teste2;
+    System.out.println("Teste3 = " + teste3);
+
     
 
     sc.close();
