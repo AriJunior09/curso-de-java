1281dc55304283ff9723ddd9be8a7cf149967ce2
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-04-14 20:10:55 -0300

    Create Aula01.java

---
+++ revisao/secao04/Aula01.java
@@ -0,0 +1,37 @@
+package revisao.secao04;
+
+public class Aula01 {
+    public static void main(String[] args) {
+
+    // Tema Principal: Processamento de dados, Casting.
+    System.out.println("Exemplo: 01");
+    int x, y;
+    x = 5;
+    y = 2 * x;
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println();
+
+
+     System.out.println("Exemplo: 02");
+    
+     int x2;
+     double y2;                // Mudamos para o tipo double
+     x2 = 5;
+     y2 = 2 * x;
+     System.out.println(x2);
+     System.out.println(y2);   // Saída do tipo double
+     System.out.println();
+
+     System.out.println("Exemplo: 03");
+    
+     int x3;
+     double y3;
+     x2 = 5;
+     y2 = 2 * x;
+     System.out.println(x2);
+     System.out.println(y2);
+     System.out.println();
+
+    }
+}
