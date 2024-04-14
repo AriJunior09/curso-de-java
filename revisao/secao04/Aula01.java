e0c33a2f900df84edbed206ca6067401daebbd46
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-04-14 20:18:24 -0300

    Update Aula01.java

--- revisao/secao04/Aula01.java
+++ revisao/secao04/Aula01.java
@@ -13,7 +13,7 @@ public class Aula01 {
     System.out.println();
 
 
-     System.out.println("Exemplo: 02");
+    System.out.println("Exemplo: 02");
     
      int x2;
      double y2;                // Mudamos para o tipo double
@@ -24,24 +24,34 @@ public class Aula01 {
      System.out.println();
 
 
-     System.out.println("Exemplo: 03");
+    System.out.println("Exemplo: 03");
     
      double b, B, h, area;       // Usando o double
      b = 6.0;
      B = 8.0;
      h = 5.0;
      area = (b + B) / 2.0 * h;
-     System.out.println(area);   // Saída do tipo double
+    System.out.println(area);   // Saída do tipo double
 
 
-     System.out.println("Exemplo: 04");
+    System.out.println("Exemplo: 03 B");
     
      float b2, B2, h2, area2;      // Usando o float
      b2 = 6f;
      B2 = 8f;
      h2 = 5f;
      area2 = (b2 + B2) / 2f * h2;
-     System.out.println(area2);    // Saída do tipo float           
+     System.out.println(area2);    // Saída do tipo float     
+     
+     
+    System.out.println("Exemplo: 04");
+    
+    int a4, b4;
+    double resultado4;
+    a4 = 5;
+    b4 = 2;
+    resultado4 = (double) a4 / b4;     // Avisando que não é para truncar e que quero o resultado em tipo double.   
+    System.out.println(resultado4);  
 
     }
 }
