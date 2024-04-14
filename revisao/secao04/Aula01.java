6d64c5aef6a58d8b57abf50a4766f1f37975c53a
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-04-14 20:14:41 -0300

    Update Aula01.java

--- revisao/secao04/Aula01.java
+++ revisao/secao04/Aula01.java
@@ -23,15 +23,25 @@ public class Aula01 {
      System.out.println(y2);   // Saída do tipo double
      System.out.println();
 
+
      System.out.println("Exemplo: 03");
     
-     int x3;
-     double y3;
-     x3 = 5;
-     y3 = 2 * x;
-     System.out.println(x3);
-     System.out.println(y3);
-     System.out.println();
+     double b, B, h, area;       // Usando o double
+     b = 6.0;
+     B = 8.0;
+     h = 5.0;
+     area = (b + B) / 2.0 * h;
+     System.out.println(area);   // Saída do tipo double
+
+
+     System.out.println("Exemplo: 04");
+    
+     float b2, B2, h2, area2;      // Usando o float
+     b2 = 6f;
+     B2 = 8f;
+     h2 = 5f;
+     area2 = (b2 + B2) / 2f * h2;
+     System.out.println(area2);    // Saída do tipo float           
 
     }
 }
