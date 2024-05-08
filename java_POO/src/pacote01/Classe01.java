efff8b157bf962564060f55c4f352541adab0328
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-08 20:58:29 -0300

    feat: update classe01

--- java_POO/src/pacote01/Classe01.java
+++ java_POO/src/pacote01/Classe01.java
@@ -7,9 +7,14 @@ public class Classe01 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		String nome = sc.next();
+		System.out.print("Nome do Aluno: ");
+		String nome = sc.nextLine();
+		System.out.print("Idade: ");
 		int idade = sc.nextInt();
-		String cor = sc.next();
+		sc.nextLine();
+		System.out.print("Cor: ");
+		String cor = sc.nextLine();
+		System.out.print("Sexo: ");
 		String sexo = sc.next();
 		
 		Aluno aluno = new Aluno(nome, idade, cor, sexo);
