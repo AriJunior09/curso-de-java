95f5af97b75c35421c9162a103139c7ee0af54fc
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-08 22:51:51 -0300

    feat: update Estabelecimento

--- java_OrientacaoObj/src/lanchonete/Estabelecimento.java
+++ java_OrientacaoObj/src/lanchonete/Estabelecimento.java
@@ -3,8 +3,46 @@ package lanchonete;
 public class Estabelecimento {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Cozinheiro cozinheiro = new Cozinheiro();
+		// Ações que não precisam estarem disponiveis para toda a aplicação
+		cozinheiro.lavarIngredientes();
+		cozinheiro.fritarIngredientesLanche();
+		cozinheiro.batendoVitaminaLiquidificador();		
+		cozinheiro.selecionarIngredientesLanche();
+		cozinheiro.selecionarIngredientesVitamina();
+		cozinheiro.prepararLanche();
+		cozinheiro.prepararVitamina();
+		cozinheiro.PrepararCombo();
+		
+		// Ações que o estabelecimento precisa ter ciência:
+		cozinheiro.adicionarLancheNoBalcao();
+		cozinheiro.adicionarVitaminaNoBalcao();
+		cozinheiro.adicionarComboNoBalcao();
+		
+		Almoxarife almoxarife = new Almoxarife();
+		// Ações que não precisam estarem disponiveis para toda a aplicação
+		almoxarife.controlarEntrada();
+		almoxarife.controlarSaida();
+		
+		// Ações que somente o ser pacote cozinha precisa conhecer(default)
+		almoxarife.entregarIngredientes();
+		almoxarife.trocarGas();
+		
+		Atendente atendente = new Atendente();
+		atendente.pegarLancheCozinha();
+		atendente.receberPagamento();
+		atendente.servindoMesa();
+		
+		// Ações que somente o ser pacote cozinha precisa conhecer(default)
+		atendente.trocarGas();
+		
+		Cliente cliente = new Cliente();
+		cliente.escolherLanche();
+		cliente.fazerPedido();
+		cliente.pagarConta();
+		
+		
+		
 	}
 
 }
