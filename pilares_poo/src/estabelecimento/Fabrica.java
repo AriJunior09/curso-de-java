1e71144a63b3775ec026278f0fecddea18f9fd9f
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-13 23:10:47 -0300

    feat: update pastas

--- pilares_poo/src/estabelecimento/Fabrica.java
+++ pilares_poo/src/estabelecimento/Fabrica.java
@@ -1,11 +1,10 @@
 package estabelecimento;
 
 import equipamentos.impressora.Impressora;
-import equipamentos.impressora.LaserJet;
 import equipamentos.multifuncional.EquipamentoMultifuncional;
 import equipamentos.copiadora.Copiadora;
 import equipamentos.digitalizadora.Digitalizadora;
-import equipamentos.impressora.DeskJet;
+
 
 public class Fabrica {
 	public static void main(String[] args) {
@@ -19,5 +18,4 @@ public class Fabrica {
 		copiadora.copiar();
 		
 	}
-
 }
