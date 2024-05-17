0ac6d2d7f552533322bbc655f0c693de0919a19f
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-16 21:43:45 -0300

    feat: update classe set

--- collections/src/collections/GenericsExemploSet.java
+++ collections/src/collections/GenericsExemploSet.java
@@ -1,9 +1,31 @@
 package collections;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class GenericsExemploSet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Exemplo sem Generics
+		Set conjuntoSemGenerics = new HashSet();
+		conjuntoSemGenerics.add("Elemento 01");
+		conjuntoSemGenerics.add(10); // Permite add qualquer tipo de obj
+
+		// Exemplo com Generics
+		Set<String> ConjuntoGenerics = new HashSet<>();
+		ConjuntoGenerics.add("Elemento A");
+		ConjuntoGenerics.add("Elemento B");
+
+		// Interando sobre a conjunto com Generics
+		for (String elemento: ConjuntoGenerics) {
+			System.out.println(elemento);
+		}
+
+		// Interando sobre o conjunto sem Generics(necessário fazer o cast)
+		for (Object elemento: conjuntoSemGenerics) {
+			String str = (String) elemento;
+			System.out.println(str);
+		}
 
 	}
 
