188711f4e41ff0842a6810d70d373fa6ce0bb28c
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-16 21:38:38 -0300

    feat: create classe set

--- collections/src/collections/GenericsExemploList.java
+++ collections/src/collections/GenericsExemploList.java
@@ -1,9 +1,32 @@
 package collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericsExemploList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Exemplo sem Generics
+		List listaSemGenerics = new ArrayList();
+		listaSemGenerics.add("Elemento 01");
+		listaSemGenerics.add(10);// Permite add qualquer tipo de obj
+		
+		// Exemplo com Generics
+		List<String> listaGenerics = new ArrayList<>();
+		listaGenerics.add("Elemento A");
+		listaGenerics.add("Elemento B");
+		
+		// Interando sobre a lista com Generics
+		for (String elemento: listaGenerics) {
+			System.out.println(elemento);
+		}
+		
+		// Interando sobre a lista sem Generics(necessário fazer o cast)
+		for (Object elemento: listaSemGenerics) {
+			String str = (String) elemento;
+			System.out.println(str);
+		}
+		
 
 	}
 
