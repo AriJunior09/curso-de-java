29be685afa5ccdda337c8ef8c8f52329b0c921a2
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-16 22:21:08 -0300

    feat: update exemplo

--- collections/src/collections/GenericsExemploList.java
+++ collections/src/collections/GenericsExemploList.java
@@ -13,25 +13,36 @@ public class GenericsExemploList {
 		listaSemGenerics.add(15.09);
 
 		// Exemplo com Generics
-		List<String> listaGenerics = new ArrayList<>();
-		listaGenerics.add("Aqui temos uma String");
-		listaGenerics.add("Elemento 2");
-		listaGenerics.add("Abacaxi tem um sabor delicioso");
+		List<String> listaGenericsSt = new ArrayList<>();
+		listaGenericsSt.add("Aqui temos uma String");
+		listaGenericsSt.add("Elemento 2");
+		listaGenericsSt.add("Abacaxi tem um sabor delicioso");
 
-		List<Integer> listaGenerics02 = new ArrayList<>();
-		listaGenerics02.add(337);
-		listaGenerics02.add(369);
-		listaGenerics02.add(1991);
+		List<Integer> listaGenericsInt = new ArrayList<>();
+		listaGenericsInt.add(337);
+		listaGenericsInt.add(369);
+		listaGenericsInt.add(1991);
+
+		List<Double> listaGenericsDb = new ArrayList<>();
+		listaGenericsDb.add(10.50);
+		listaGenericsDb.add(3633.99);
+		listaGenericsDb.add(14.99);
 
 		// Interando sobre a lista com Generics
 		System.out.println("\nElementos da Lista de Genericos Tipo String: ");
-		for (String elemento: listaGenerics) {
+		for (String elemento: listaGenericsSt) {
 			System.out.println(elemento);
 		}
 
 		// Interando sobre a lista com Generics02
 		System.out.println("\nElementos da Lista de Genericos Tipo Integer: ");
-		for (Integer elemento: listaGenerics02) {
+		for (Integer elemento: listaGenericsInt) {
+			System.out.println(elemento);
+		}
+
+		// Interando sobre a lista com Generics02
+		System.out.println("\nElementos da Lista de Genericos Tipo Double: ");
+		for (Double elemento: listaGenericsDb) {
 			System.out.println(elemento);
 		}
 
